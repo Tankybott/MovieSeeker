d6769970d94557b71d8b7c6639abea323b25618f
Tankybott <mbukowski663@gmail.com>
2025-06-18 19:59:34 +0200

    maked join button work on github pages

--- src/components/nav/JoinButton.tsx
+++ src/components/nav/JoinButton.tsx
@@ -1,10 +1,12 @@
+import { Link } from "react-router-dom";
+
 export default function JoinButton() {
   return (
-    <a
-      href={import.meta.env.BASE_URL + "join"}
+    <Link
+      to="/join"
       className="p-2 px-5 cursor-pointer bg-gradient bg-main-gradient text-lg lg:text-md xl:text-lg text-white rounded-lg inline-block"
     >
       Join
-    </a>
+    </Link>
   );
 }
