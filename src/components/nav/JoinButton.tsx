2bd4907bf1776346f5be63627fff3f1e294e5f69
Tankybott <mbukowski663@gmail.com>
2025-06-18 20:42:43 +0200

    small fix

--- src/components/nav/JoinButton.tsx
+++ src/components/nav/JoinButton.tsx
@@ -1,12 +1,12 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 export default function JoinButton() {
   return (
-    <Link
+    <NavLink
       to="/join"
       className="p-2 px-5 cursor-pointer bg-gradient bg-main-gradient text-lg lg:text-md xl:text-lg text-white rounded-lg inline-block"
     >
       Join
-    </Link>
+    </NavLink>
   );
 }
